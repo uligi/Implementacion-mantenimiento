7f246c763a8891454289d18a2b78d95fa2a8513e
uligi <59987716+uligi@users.noreply.github.com>
2024-11-24 11:54:16 -0600

    inicio

---
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Implementacion_Mantenimiento
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Implementacion_Mantenimiento.MvcApplication" Language="C#" %>
