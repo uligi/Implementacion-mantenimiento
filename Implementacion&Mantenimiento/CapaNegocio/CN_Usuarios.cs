9b93ca50cbdfa776958d6b757fd4c80a0ef20185
uligi <59987716+uligi@users.noreply.github.com>
2024-11-27 10:53:27 -0600

    d

--- Implementacion&Mantenimiento/CapaNegocio/CN_Usuarios.cs
+++ Implementacion&Mantenimiento/CapaNegocio/CN_Usuarios.cs
@@ -20,6 +20,10 @@ namespace CapaNegocio
 
         public int Registrar(Usuarios obj, out string Mensaje)
         {
+            string clave = CN_Recursos.GenerarClave();
+            obj.Contrasena = CN_Recursos.ConvertirSha256(clave); // Hash password
+            int resultado = objCapaDato.Registrar(obj, out Mensaje);
+
             if (obj.oPersonas == null || string.IsNullOrWhiteSpace(obj.oPersonas.NombreCompleto))
             {
                 Mensaje = "El nombre completo de la persona es obligatorio.";
@@ -33,6 +37,60 @@ namespace CapaNegocio
             }
 
             return objCapaDato.Registrar(obj, out Mensaje);
+        }
+
+        public bool Editar(Usuarios obj, out string Mensaje)
+        {
+            return objCapaDato.Editar(obj, out Mensaje);
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+
+        public bool CambiarClave(int UsuarioID, string nuevaClave, out string Mensaje)
+        {
+            return objCapaDato.CambiarClave(UsuarioID, nuevaClave, out Mensaje);
+        }
+
+        public bool DesactivarUsuario(int id, out string Mensaje)
+        {
+            return objCapaDato.DesactivarUsuario(id, out Mensaje);
+        }
+
+        public bool RestablecerContrasena(int UsuarioID, string correo, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+            string nuevaClave = CN_Recursos.GenerarClave();
+            bool resultado = objCapaDato.RestablecerContrasena(UsuarioID, CN_Recursos.ConvertirSha256(nuevaClave), out Mensaje);
+
+            if (resultado)
+            {
+                string asunto = "Contraseña Reestablecida";
+                string mensaje_correo = "<h3>Su cuenta fue reestablecida correctamente</h3><br><p>Su contraseña para acceder ahora es: !clave!</p>";
+                mensaje_correo = mensaje_correo.Replace("!clave!", nuevaClave);
+
+                bool respuesta = CN_Recursos.EnviarCorreo(correo, asunto, mensaje_correo);
+
+                if (respuesta)
+                {
+                    return true;
+                }
+                else
+                {
+                    Mensaje = "No se pudo enviar el correo";
+                    return false;
+                }
+            }
+            else
+            {
+                Mensaje = "No se pudo reestablecer la contrasena";
+                return false;
+
+            }
+
+
         }
 
     }
--- Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/AccesoController.cs
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/AccesoController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CapaEntidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using CapaNegocio;
 
 namespace Implementacion_Mantenimiento.Controllers
 {
@@ -21,6 +24,93 @@ namespace Implementacion_Mantenimiento.Controllers
         public ActionResult RestablecerContrasena()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string correo, string clave)
+        {
+            if (string.IsNullOrEmpty(correo) || string.IsNullOrEmpty(clave))
+            {
+                ViewBag.Error = "Correo y contraseña no pueden estar vacíos.";
+                return View();
+            }
+
+
+            string hashedClave = CN_Recursos.ConvertirSha256(clave);
+
+
+            Usuarios oUsuario = new CN_Usuario().Listar()
+                .Where(u => u.Persona.Correo.DireccionCorreo == correo && u.Contrasena == hashedClave).FirstOrDefault();
+
+            if (oUsuario == null)
+            {
+                ViewBag.Error = "Correo o contraseña incorrectos";
+                return View();
+            }
+            else
+            {
+                if (oUsuario.RestablecerContrasena)
+                {
+                    TempData["UsuarioID"] = oUsuario.UsuarioID;
+                    return RedirectToAction("CambiarClave");
+                }
+                FormsAuthentication.SetAuthCookie(oUsuario.Personas.Correo, false);
+                Session["NombreUsuario"] = oUsuario.Personas.Nombre;
+                Session["Rol"] = oUsuario.Roles.Rol;
+                Session["UsuarioID"] = oUsuario.UsuarioID;
+                ViewBag.Error = null;
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CambiarClave(string UsuarioID, string claveActual, string nuevaClave, string confirmarClave)
+        {
+
+            Usuarios oUsuario = new Usuarios();
+            oUsuario = new CN_Usuarios().Listar()
+                .Where(u => u.UsuarioID == int.Parse(UsuarioID)).FirstOrDefault();
+            if (oUsuario.Contrasena != CN_Recursos.ConvertirSha256(claveActual))
+            {
+                TempData["UsuarioID"] = UsuarioID;
+                ViewData["vClave"] = "";
+                ViewBag.Error = "La contraseña actual no es correcta";
+                return View();
+            }
+            else if (nuevaClave != confirmarClave)
+            {
+                TempData["UsuarioID"] = UsuarioID;
+                ViewData["vClave"] = claveActual;
+                ViewBag.Error = "Las contraseñas no coinciden";
+                return View();
+            }
+            ViewData["vClave"] = "";
+            nuevaClave = CN_Recursos.ConvertirSha256(nuevaClave);
+
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Usuarios().CambiarClave(int.Parse(UsuarioID), nuevaClave, out mensaje);
+
+            if (respuesta)
+            {
+                return RedirectToAction("Index", "Acceso");
+            }
+            else
+            {
+                TempData["UsuarioID"] = UsuarioID;
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+        }
+
+
+        public ActionResult CerrarSesion()
+        {
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Acceso");
         }
     }
 }
