db6c4694de08a0eb3e5018185620668566bcb338
uligi <59987716+uligi@users.noreply.github.com>
2024-11-28 15:16:01 -0600

    d

--- Implementacion&Mantenimiento/Implementacion&Mantenimiento/App_Start/BundleConfig.cs
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/App_Start/BundleConfig.cs
@@ -8,30 +8,22 @@ namespace Implementacion_Mantenimiento
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new Bundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-3.7.0.js",
-                         "~/Scripts/jquery.js",
-                          "~/Scripts/jquery.easing.min.js",
-                        "~/Scripts/DataTables/jquery.dataTables.js",
-                        "~/Scripts/DataTables/dataTables.responsive.js",
-                        "~/Scripts/Validaciones.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                      "~/Scripts/jquery-{version}.js",
+                      "~/Scripts/jquery-3.7.0.js",
+                      "~/Scripts/DataTables/jquery.dataTables.js",
+                      "~/Scripts/DataTables/dataTables.responsive.js",
+                      "~/Scripts/Validaciones.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/complementos").Include(
-                      "~/Scripts/fontawesome/all.min.js",
-                      "~/Scripts/scripts.js",
-                      "~/Scripts/loadingoverlay/loadingoverlay.min.js",
-                      "~/Scripts/sweetalert.min.js"
+                        "~/Scripts/fontawesome/all.min.js",
+                        "~/Scripts/scripts.js",
+                        "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                        "~/Scripts/sweetalert.min.js"
 
-           ));
+             ));
 
-            /*        bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                                "~/Scripts/jquery.validate*"));
-
-                    // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-                    // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-                    bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                                "~/Scripts/modernizr-*"));*/
+          
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.bundle.min.js"));
