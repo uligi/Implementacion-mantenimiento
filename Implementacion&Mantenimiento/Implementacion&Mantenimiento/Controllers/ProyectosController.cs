ef3adcc20f96c86a03141a39836b66529ce67ae8
uligi <59987716+uligi@users.noreply.github.com>
2024-12-11 16:17:32 -0600

    reportes

--- Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/ProyectosController.cs
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/ProyectosController.cs
@@ -2,6 +2,8 @@
 using CapaNegocio;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using OfficeOpenXml;
+using System;
 
 namespace Implementacion_Mantenimiento.Controllers
 {
@@ -60,5 +62,69 @@ namespace Implementacion_Mantenimiento.Controllers
             var lista = _proyectosNegocio.ObtenerReporteProyectos();
             return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
         }
+
+
+        public FileResult ExportarReporteProyectos()
+        {
+            // Configurar el contexto de licencia para EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Obtener los datos del reporte
+            var lista = _proyectosNegocio.ObtenerReporteProyectos();
+
+            // Crear el archivo Excel
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Reporte de Proyectos");
+
+                // Encabezados
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Nombre";
+                worksheet.Cells[1, 3].Value = "Descripción";
+                worksheet.Cells[1, 4].Value = "Estado";
+                worksheet.Cells[1, 5].Value = "Fecha Inicio";
+                worksheet.Cells[1, 6].Value = "Fecha Fin";
+                worksheet.Cells[1, 7].Value = "Activo";
+
+                // Insertar datos
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    var proyecto = lista[i];
+                    worksheet.Cells[i + 2, 1].Value = proyecto.ProyectoID;
+                    worksheet.Cells[i + 2, 2].Value = proyecto.Nombre;
+                    worksheet.Cells[i + 2, 3].Value = proyecto.Descripcion;
+                    worksheet.Cells[i + 2, 4].Value = proyecto.Estado;
+
+                    if (proyecto.FechaInicio is DateTime fechaInicio)
+                    {
+                        worksheet.Cells[i + 2, 5].Value = fechaInicio.ToString("yyyy-MM-dd");
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, 5].Value = "Sin fecha";
+                    }
+
+                    if (proyecto.FechaFin is DateTime fechaFin)
+                    {
+                        worksheet.Cells[i + 2, 6].Value = fechaFin.ToString("yyyy-MM-dd");
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, 6].Value = "Sin fecha";
+                    }
+
+                    worksheet.Cells[i + 2, 7].Value = proyecto.Activo ? "Sí" : "No";
+                }
+
+                // Ajustar columnas automáticamente
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                // Retornar el archivo Excel
+                var excelData = package.GetAsByteArray();
+                return File(excelData, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ReporteProyectos.xlsx");
+            }
+        }
+
+
     }
 }
--- Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/TareasController.cs
+++ Implementacion&Mantenimiento/Implementacion&Mantenimiento/Controllers/TareasController.cs
@@ -2,7 +2,8 @@
 using CapaNegocio;
 using System.Collections.Generic;
 using System.Web.Mvc;
-
+using OfficeOpenXml;
+using System;
 namespace Implementacion_Mantenimiento.Controllers
 {
     [Authorize]
@@ -60,6 +61,67 @@ namespace Implementacion_Mantenimiento.Controllers
         {
             List<Tareas> lista = cn_tareas.ReporteTareas();
             return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+        public FileResult ExportarReporteTareas()
+        {
+            // Configurar el contexto de licencia para EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Obtener los datos del reporte
+            var lista = cn_tareas.ReporteTareas();
+
+            // Crear el archivo Excel
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Reporte de Tareas");
+
+                // Encabezados
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Descripción";
+                worksheet.Cells[1, 3].Value = "Estado";
+                worksheet.Cells[1, 4].Value = "Fecha Inicio";
+                worksheet.Cells[1, 5].Value = "Fecha Fin";
+                worksheet.Cells[1, 6].Value = "Proyecto";
+                worksheet.Cells[1, 7].Value = "Asignado a";
+
+                // Insertar datos
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    var tarea = lista[i];
+                    worksheet.Cells[i + 2, 1].Value = tarea.TareaID;
+                    worksheet.Cells[i + 2, 2].Value = tarea.Descripcion;
+                    worksheet.Cells[i + 2, 3].Value = tarea.Estado;
+
+                    if (tarea.FechaInicio is DateTime fechaInicio)
+                    {
+                        worksheet.Cells[i + 2, 4].Value = fechaInicio.ToString("yyyy-MM-dd");
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, 4].Value = "Sin fecha";
+                    }
+
+                    if (tarea.FechaFin is DateTime fechaFin)
+                    {
+                        worksheet.Cells[i + 2, 5].Value = fechaFin.ToString("yyyy-MM-dd");
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, 5].Value = "Sin fecha";
+                    }
+
+                    worksheet.Cells[i + 2, 6].Value = tarea.oProyectos?.Nombre ?? "Sin Proyecto";
+                    worksheet.Cells[i + 2, 7].Value = tarea.oUsuarios?.oPersonas?.NombreCompleto ?? "Sin Asignar";
+                }
+
+                // Ajustar columnas automáticamente
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                // Retornar el archivo Excel
+                var excelData = package.GetAsByteArray();
+                return File(excelData, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ReporteTareas.xlsx");
+            }
         }
     }
 }
